73a3c3f22758788e61db621e13a133888970dda3
ziadmohamed0 <zizo.alprnc.90@gmail.com>
2024-10-28 21:42:29 +0300

    init all Drivers

---
+++ ATmega2560/Debug/App/main.d
@@ -0,0 +1,3 @@
+App/main.o App/main.o: ../App/main.c ../App/main.h
+
+../App/main.h:
---
+++ Atmega32/App/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: 28/10/2024
+ *      Author: Ziad Mohammed
+ */
+#include "main.h"
+
+int main(void) {
+
+	while (1) {
+
+	}
+	return 0;
+}
+
